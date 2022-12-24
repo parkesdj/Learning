ae0cacfafff0136c33c87d094f455a7ed3c06aed
parkesdj <parkesdj.dp@gmail.com>
2022-12-24 16:59:55 +0000

    Initial Commit

---
+++ TupleFindValue.py
@@ -0,0 +1,11 @@
+"""
+Access value 20 from the tuple
+The given tuple is a nested tuple. write a Python program to print the value 20.
+
+Given: tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+"""
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+print(len(tuple1))
+
+print(tuple1[1][1])
