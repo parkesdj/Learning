e94c64d2e52f8f913a5f53bd7119ddbc2918ee48
parkesdj <parkesdj.dp@gmail.com>
2022-12-24 12:07:51 +0000

    Initial Commit

---
+++ FuncLargestNumber.py
@@ -0,0 +1,13 @@
+"""
+Find the largest item from a given list
+>>list --> number
+>>[1, 23, 897, 43, 11, 57] --> 897
+"""
+data = [1, 23, 897, 43, 11, 57]
+
+
+def largest(data) :
+    print(max(data))
+
+
+largest(data)
